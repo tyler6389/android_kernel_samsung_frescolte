b5f1607634e7fd882a4121eebc470fbeb940fef7
tyler6389 <tyler6389@gmail.com>
2015-07-25 20:32:51 +0900

    first commit

---
+++ build_kernel.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export ARCH=arm
+export CROSS_COMPILE=/opt/toolchains/arm-eabi-4.6/bin/arm-eabi-
+mkdir output
+
+make -C $(pwd) O=output msm8974_sec_defconfig VARIANT_DEFCONFIG=msm8974_sec_frescolte_skt_defconfig SELINUX_DEFCONFIG=selinux_defconfig
+make -C $(pwd) O=output
+
+cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
+
+echo Samsung Lollipop Kernel Source
+echo By Count
